1c8b1cd6ba6cd3986dcb9b6bd11ae3e9339ed40d
rowan <rowan@g1project.net>
2023-04-28 02:40:35 +0900

    deploy 에러 개선 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express')
 const app = express()
-const http = require('node:http')
+const http = require('http')
 const server = http.createServer(app)
 const socket = require('socket.io')
 const io = socket(server)
